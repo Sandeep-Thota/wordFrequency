a8a6beedc8f897cc46b57a1aa43ef7476d3efe1a
Sandeep <sandeepthota3230@gmail.com>
2020-01-23 01:32:00 -0500

    Initial commit

---
+++ wordsworkspace/wordfrequency/src/main/java/com/words/wordfrequency/App.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Jan 23 2020
+ */
+package com.words.wordfrequency;
+
+/** @author Sai Naveen Kavuri **/
+public class App {
+	public static void main(String[] args) {
+		FileWord file = new FileWord();
+		file.wordDocument();
+	}
+
+}
